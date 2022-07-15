d5c3344ed633e29afbd278bd505c814fad27362b
Aung Kyi Thein <aungkyithein.it@gmail.com>
2022-07-15 10:47:26 +0630

    ---

--- python-flask/usingENVariables/client/client.py
+++ python-flask/usingENVariables/client/client.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 import requests
 import os
 app = Flask(__name__)
-
+flaskapi = os.getenv['flaskapi']
 @app.route('/')
 def hello():
     try:
-        x = requests.get("http://{flaskapi}:5000")
+        x = requests.get(f"http://{flaskapi}:5000")
         return render_template('index.html', data = x.json())
     except:
         return render_template('index.html', data = 'API server is Down')
