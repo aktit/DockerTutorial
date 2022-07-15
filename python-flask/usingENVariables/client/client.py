1ea3196fb54daf93d243f8299e0b7e8435330bec
Aung Kyi Thein <aungkyithein.it@gmail.com>
2022-07-15 14:24:56 +0630

    ---

--- python-flask/usingENVariables/client/client.py
+++ python-flask/usingENVariables/client/client.py
@@ -4,17 +4,23 @@ import os
 app = Flask(__name__)
 url = os.environ['URL']
 @app.route('/')
+
+# def hello():
+#     for i in range(1000):
+#         print(f"Hello {url}")
+#     try:
+#         x = requests.get(url)
+#         return render_template('index.html', data = x.json())
+#     except:
+#         return render_template('index.html', data = 'API server is Down')
+
+# app.run(host='0.0.0.0',port=10000)
+
 def hello():
-    for i in range(1000):
-        print(f"Hello {url}")
-    try:
-        x = requests.get(url)
-        return render_template('index.html', data = x.json())
-    except:
-        return render_template('index.html', data = 'API server is Down')
+    x = requests.get(url)
+    return render_template('index.html', data = x.json())
 
 app.run(host='0.0.0.0',port=10000)
-
 
 
 
