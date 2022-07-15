b0c0cd831d9deb48f9a70bcf332dad53569ab6e7
Aung Kyi Thein <aungkyithein.it@gmail.com>
2022-07-15 10:37:18 +0630

    Testing ENV

---
+++ python-flask/testing.sql
@@ -0,0 +1,7 @@
+USE testing;
+CREATE TABLE employees(
+emp_no      INT             NOT NULL AUTO_INCREMENT, 
+first_name  VARCHAR(14)     NOT NULL,
+last_name   VARCHAR(16)     NOT NULL,
+PRIMARY KEY(emp_no)
+);
